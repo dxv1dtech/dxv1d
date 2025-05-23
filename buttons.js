50ea2272518730ef8f2ca82171c56c3e61f65787
dxv1dtech <petrache_david_andrei@yahoo.com>
2025-05-23 19:22:30 +0300

    .

--- buttons.js
+++ buttons.js
@@ -1,6 +1,8 @@
 const button1 = document.getElementById("home");
 const button2 = document.getElementById("aboutme");
+const epilepsy = document.getElementById("epilepsy")
 const div = document.getElementById("introduction");
+const video = document.getElementById("bg-video")
 
 button2.addEventListener("click", (e) => {
     if (div.style.display === "none") {
@@ -12,4 +14,10 @@ button2.addEventListener("click", (e) => {
 
 button1.addEventListener("click", (e) => {
     window.location.href = "index.html";
+});
+
+epilepsy.addEventListener("click", (e) => {
+    document.getElementById("warningOverlay").style.display = "none";
+    document.getElementById("content").style.display = "block";
+    video.play();
 });
